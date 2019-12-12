df9d82f7ab509afeef65d0bf9935fbeec6ef47fd
Yaroslav-Melnychenko <yasha271828@gmail.com>
2019-12-12 13:20:17 +0200

    add files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $('.slider').owlCarousel({
+        margin:10,
+        autoplay:true,
+        loop:true,
+        autoplayTimeout:3000,
+        nav:true,
+        dots:true,
+        items:1
+    });
+  
+});
